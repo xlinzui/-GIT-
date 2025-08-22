3af423fb71b1da9131851990c24b6a2b76167b63
tangziyi <u202312842@hust.edu.cn>
2025-08-22 12:43:45 +0800

    feat: implement main user interface with all shape stubs

---
+++ 用户交互/Circle.cpp
@@ -0,0 +1,10 @@
+#include "Circle.h"
+
+Circle::Circle(double radius, char unit) {
+    // 占位符实现
+}
+
+double Circle::calculateArea() {
+    // 占位符实现
+    return -1.0;
+}
---
+++ 用户交互/Triangle.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+class Triangle {
+public:
+    Triangle(double base, double height, char unit);
+    double calculateArea();
+};
+
+#endif
